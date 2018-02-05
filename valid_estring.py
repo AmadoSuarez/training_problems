7a83e1c27683850a206a80eed1339f7663b167f6
Amado Suarez <amado.suarez07@gmail.com>
2018-02-05 01:07:37 -0500

    Create valid_estring.py

---
+++ valid_estring.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import sys
+
+
+def isValid(s):
+    # Complete this function
+    unique_chars = list(set(s))
+    char_freq = {}
+    for char in unique_chars:
+        char_freq[char] = s.count(char)
+        
+    freq = list(char_freq.values())
+    freq_set = set(freq)
+  
+    #print(unique_chars)
+    #print(char_freq)
+    #print(freq)
+    
+    
+    if min(freq)==max(freq) :
+        return "YES"
+    elif (max(freq) - 1) == (min(freq)) and freq.count(max(freq)) == 1 and len(freq_set) <= 2:
+        return "YES"
+    
+    elif len(freq_set) == 2 and min(freq_set) == 1:
+        freq.remove(1)
+        if len(set(freq)) == 1:
+            return "YES"
+        else:
+            return "NO"
+    else:
+         return "NO"
+        
+        
+s = input().strip()
+result = isValid(s)
+print(result)
